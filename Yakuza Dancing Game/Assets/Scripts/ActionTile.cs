cc30aada19e54f22011d90e77c102699b1395b45
Dominik <endrych.d@seznam.cz>
2023-01-29 13:30:57 +0100

    Add spawning actions
    Actions that plalyer can interact with now spawn across all the tiles.
    Actions have starting animation and disappear after its end.

---
+++ Yakuza Dancing Game/Assets/Scripts/ActionTile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+public class ActionTile : MonoBehaviour
+{
+    public event EventHandler<ActionTile> ActionCompleted;    // Event handler for when this action is completed
+    public Tile Tile;
+
+    private ActionIcon _actionIcon;
+    
+
+    public void SetTile(Tile tile)
+    {
+        Tile = tile;
+    }
+
+    private void OnActionCompleted()
+    {
+        ActionCompleted.Invoke(this, gameObject.GetComponent<ActionTile>());      // Invoke ActionCompleted event
+    }
+}
---
+++ Yakuza Dancing Game/Assets/Scripts/ActionsController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ActionsController : MonoBehaviour
+{
+    [SerializeField] List<Tile> _availableTiles;
+    [SerializeField] Transform _actionsParent;
+    [SerializeField] GameObject _actionPrefab;
+    [SerializeField] Transform _player;
+
+    private Coroutine _spawnCoroutine;
+    private bool _continue;
+
+    [SerializeField] List<ActionTile> _actionTiles;
+
+    private void Start()
+    {
+        // TODO: Subscribe this to OnGameStart event
+        StartSpawning();
+    }
+
+    // Start spawning
+    public void StartSpawning()
+    {
+        _continue = true;
+        _spawnCoroutine = StartCoroutine(SpawnRoutine());
+    }
+
+    // Stop spawning
+    private void StopSpawning()
+    {
+        _continue = false;
+        StopCoroutine(_spawnCoroutine);
+        _spawnCoroutine = null;
+    }
+
+    private Tile GetRandomAvailableTile()
+    {
+        int idx = Random.Range(0, _availableTiles.Count);
+        return _availableTiles[idx];
+    }
+
+    private IEnumerator SpawnRoutine()
+    {
+        while(_continue)
+        {
+            Tile tile = GetRandomAvailableTile();   // Get tile for this action
+            _availableTiles.Remove(tile);           // Remove tile from available tiles
+
+            GameObject actionTileObj = Instantiate(_actionPrefab, tile.transform.position, Quaternion.identity, _actionsParent);    // Instantiate new action tile
+
+            actionTileObj.GetComponent<ActionTile>().SetTile(tile);
+            actionTileObj.GetComponent<ActionTile>().ActionCompleted += TileActionCompleted;    // Subscribe to ActionCompleted event on currently spawned action tile
+
+            _actionTiles.Add(actionTileObj.GetComponent<ActionTile>());
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    // ActionCompleted event handler
+    private void TileActionCompleted(object sender, ActionTile actionTile)
+    {
+        _availableTiles.Add(actionTile.Tile);
+        _actionTiles.Remove(actionTile);
+        Destroy(actionTile.gameObject);
+    }
+
+    public void Action1(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            // Iterate over action tiles and check if player is standing on one
+            foreach (ActionTile actionTile in _actionTiles)
+            {
+                if (actionTile.transform.position == _player.position)
+                {
+                    Debug.Log("Action success");
+                    break;
+                }
+            }
+        }
+        
+    }
+
+}
